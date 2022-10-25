33f8a7340137b915ce9cc4427a5217fe864ac96d
Experiment2601 <aniketgavas18@gmail.com>
2022-10-26 00:07:30 +0530

    Employee_Wage_UC9

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,10 +3,19 @@ package com.bridelabz.employeewage;
 public class EmployeeWage {
     public static final int FULL_TIME = 1;
     public static final int PART_TIME = 2;
+    private final String company;
+    private final int empPerHourRate;
+    private final int numOfWorkingDays;
+    private final int maxHourInMonth;
+    private int totalEmpWage;
 
-
-    //Refactor the code
-    public static int computeEmpWage(String company, int empPerHourRate, int numOfWorkingDays, int maxHourInMonth) {
+    public EmployeeWage(String company, int empPerHourRate, int numOfWorkingDays, int maxHourInMonth){
+        this.company = company;
+        this.empPerHourRate = empPerHourRate;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHourInMonth = maxHourInMonth;
+    }
+    public void computeWage() {
         int empHrs = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
@@ -27,17 +36,26 @@ public class EmployeeWage {
             totalEmpHrs += empHrs;
             System.out.println("Day: "+totalWorkingDays+ "Employee Hour: "+empHrs);
         }
-        int totalEmpWage = totalEmpHrs * empPerHourRate;
-        System.out.println("Total Employee wage for Company: "+company+ "is: "+totalEmpWage);
-        return totalEmpWage;
+        totalEmpWage = totalEmpHrs * empPerHourRate;
     }
-
+    @Override
+    public String toString(){
+        return "Total Employee Wage For Company: "+company+ "is: "+totalEmpWage;
+    }
     //Employee Wage For Multiple Companies
 
     public static void main(String[] args) {
-        computeEmpWage("Jio",30,22,12);
-        computeEmpWage("Accenture",50,20,9);
-        computeEmpWage("NSEIT",60,24,10);
-        computeEmpWage("Infosys",45,26,11);
+        EmployeeWage jio = new EmployeeWage("Jio",30,22,12);
+        EmployeeWage accenture = new EmployeeWage("Accenture",50,20,9);
+        EmployeeWage nseit = new EmployeeWage("NSEIT",60,24,10);
+        EmployeeWage infosys = new EmployeeWage("Infosys",45,26,11);
+        jio.computeWage();
+        System.out.println(jio);
+        accenture.computeWage();
+        System.out.println(accenture);
+        nseit.computeWage();
+        System.out.println(nseit);
+        infosys.computeWage();
+        System.out.println(infosys);
     }
 }
